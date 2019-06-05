e8997438cbe4d204baef3db4426baf8dadb51d20
bensongathee <benson7484@gmail.com>
2019-06-05 00:16:38 -0400

    updated files

---
+++ Portfolio/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PersonalPortfolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Projects()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+
+            return View();
+        }
+        public IActionResult Articles()
+        {
+            return View();
+        }
+    }
+}
